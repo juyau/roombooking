1f7b1892749af29624b647762afd9cf48c7e68cf
Sydneyfullstack <samuelju2017@gmail.com>
2021-09-13 21:30:54 +1000

    edit common

--- roombooking-common/src/main/java/org/thebreak/roombooking/common/Constants.java
+++ roombooking-common/src/main/java/org/thebreak/roombooking/common/Constants.java
@@ -4,7 +4,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class Constants {
-    // test text2222
+    // test text2222666
     public static final int DEFAULT_PAGE_SIZE = 10;
     public static final int MAX_PAGE_SIZE = 50;
     public static final int START_TIME_OF_DAY = 5;
