e3b8ebc912d24527d6b4d9ca5cf3f41e0db38451
Sydneyfullstack <samuelju2017@gmail.com>
2021-09-13 19:36:38 +1000

    change common2

--- roombooking-app/src/main/java/org/thebreak/roombooking/app/controller/EmptyTestFile.java
+++ roombooking-app/src/main/java/org/thebreak/roombooking/app/controller/EmptyTestFile.java
@@ -2,6 +2,6 @@ package org.thebreak.roombooking.app.controller;
 
 public class EmptyTestFile {
     public String getString(){
-        return "test22255555666666";
+        return "test2225555566666677777";
     }
 }
