e9138377d4a2aca63c6cb9c55252403ca9c43e6c
Sydneyfullstack <samuelju2017@gmail.com>
2021-09-14 15:21:48 +1000

    test gateway hook

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/sh
 #receive pass in params
 docker_user=$1
 project_name=$2
@@ -12,31 +12,31 @@ echo "check if container exists"
 containerid=`docker ps -a | grep $project_name:$tag | awk '{print $1}'`
 if [ $containerid != "" ]
 then
-  echo "container exists, stopping..."
-  docker stop $containerid
-  echo "delete container..."
-  docker rm $containerid
-  echo "container removed success."
+echo "container exists, stopping..."
+docker stop $containerid
+echo "delete container..."
+docker rm $containerid
+echo "container removed success."
 fi
 
 #check if image exists and delete
 imageid=`docker images | grep $project_name | awk '{print $3}'`
 if [ $imageid != "" ]
 then
-  echo "image exists, deleting..."
-  docker rmi -f $imageid
-  echo "image deleted success."
+echo "image exists, deleting..."
+docker rmi -f $imageid
+echo "image deleted success."
 fi
 
 #download image
 docker pull $docker_user/$project_name:$tag
 
 #only gateway need to expose port to host machine
-if [ $project_name == "gateway"]
+if [ $project_name == "gateway" ]
 then
-  docker run --network $network -di -p $port:$port $image:$tag
+docker run --name $project_name --network $network -d -p $port:$port $docker_user/$project_name:$tag
 else
-  docker run --network $network -di $image:$tag
+docker run --name $project_name --network $network -d $docker_user/$project_name:$tag
 fi
 
 echo "container running on network - '$network'"
