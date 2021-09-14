05e5ad6150f3b20226fc8abd0ed310af66c19910
Sydneyfullstack <samuelju2017@gmail.com>
2021-09-14 14:29:17 +1000

    test gateway hook

--- gateway/src/main/java/org/thebreak/roombooking/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/thebreak/roombooking/gateway/GatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
+        // test
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
