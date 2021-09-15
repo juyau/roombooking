a2fd4dfdbb2b7226588b45f97c7d038ad3765b6e
Sydneyfullstack <samuelju2017@gmail.com>
2021-09-15 12:17:55 +1000

    update gateway test2

--- deploy.sh
+++ deploy.sh
@@ -9,34 +9,30 @@ port=$5
 echo "check if container exists"
 
 #check if container running and stop then delete
-containerid=`docker ps -a | grep $project_name:$tag | awk '{print $1}'`
-if [ $containerid != "" ]
+containerId=$(docker ps -a | grep "$project_name":"$tag" | awk '{print $1}')
+if [ "$containerId" != "" ]
 then
 echo "container exists, stopping..."
-docker stop $containerid
+docker stop "$containerId"
 echo "delete container..."
-docker rm $containerid
+docker rm "$containerId"
 echo "container removed success."
 fi
 
 #check if image exists and delete
-imageid=`docker images | grep $project_name | awk '{print $3}'`
-if [ $imageid != "" ]
-
+imageId=$(docker images | grep "$project_name" | awk '{print $3}')
+if [ "$imageId" != "" ]
+then
 echo "image exists, deleting..."
-docker rmi -f $imageid
+docker rmi -f "$imageId"
 echo "image deleted success."
 fi
 
 #download image
-docker pull $docker_user/$project_name:$tag
+docker pull "$docker_user"/"$project_name":"$tag"
 
 #run docker container
-if [ $project_name == "gateway" ]
-then
-docker run --name $project_name --network $network -d -p $port:$port $docker_user/$project_name:$tag
-else
-docker run --name $project_name --network $network -d $docker_user/$project_name:$tag
-fi
+docker run --name "$project_name" --network "$network" -d -p "$port":"$port" "$docker_user"/"$project_name":"$tag"
+
 
 echo "container running on network - '$network'"
--- gateway/src/main/java/org/thebreak/roombooking/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/thebreak/roombooking/gateway/GatewayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
-        // test 2234444
+        // test 2234444555
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
