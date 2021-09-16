e3d24b559b0ae402fd410489271b1301a6338960
Sydneyfullstack <samuelju2017@gmail.com>
2021-09-16 16:21:19 +1000

    update gateway test2

--- gateway/src/main/java/org/thebreak/roombooking/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/thebreak/roombooking/gateway/GatewayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
-        // test 223
+        // test 2234
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
